812bdc9229a8e8ca3a9aa94e3ef9bc706cf6ab9c
B0R0koko <mvmironov@edu.hse.ru>
2021-10-10 16:44:44 +0300

    test

--- mainapp/__init__.py
+++ mainapp/__init__.py
@@ -8,7 +8,7 @@ Your app description
 
 class Constants(BaseConstants):
     name_in_url = "mainapp"
-    players_per_group = None
+    players_per_group = 2
     num_rounds = 1
     payment = cu(1)
 
