0c81467af7426e91068ea74be5e1349864c4f5ef
B0R0koko <mvmironov@edu.hse.ru>
2021-10-11 23:50:14 +0300

    final commit i hope so

--- mainapp/__init__.py
+++ mainapp/__init__.py
@@ -11,6 +11,14 @@ class Constants(BaseConstants):
     players_per_group = 2
     num_rounds = 1
     payment = cu(1)
+    questions = [
+        "Угадайте расстояние между Сан-Франциско и Иваново. Укажите ответ в километрах",
+        "Угадайте расстояние между Костромой и Норильском",
+        "Угадайте расстояние между Находкой и Владикавказом",
+        "Угадайте расстояние между Чикаго и Якутском",
+        "Угадайте расстояние между Осло и Астраханью",
+    ]
+    answers = [9362, 2579, 6814, 7984, 2852]
 
 
 class Subsession(BaseSubsession):
@@ -23,12 +31,28 @@ class Group(BaseGroup):
 
 class Player(BasePlayer):
 
-    num_1 = models.FloatField()
-    num_2 = models.FloatField()
-    num_3 = models.FloatField()
-    num_4 = models.FloatField()
-    num_5 = models.FloatField()
+    num_1 = models.FloatField(label="Введите ответ сюда:")
+    num_2 = models.FloatField(label="Введите ответ сюда:")
+    num_3 = models.FloatField(label="Введите ответ сюда:")
+    num_4 = models.FloatField(label="Введите ответ сюда:")
+    num_5 = models.FloatField(label="Введите ответ сюда:")
     total_payoff = models.FloatField(initial=0)
+    options = models.IntegerField(
+        choices=[
+            [0, "0 очков мне и 500 очков сопернику"],
+            [1, "100 очков мне и 400 очков сопернику."],
+            [2, "200 очков мне и 300 очков сопернику."],
+            [3, "300 очков мне и 200 очков сопернику"],
+            [4, "400 очков мне и 100 очков сопернику"],
+            [5, "500 очков мне и 0 очков сопернику."],
+        ],
+        widget=widgets.RadioSelect,
+        label="Варианты:",
+    )
+
+    # Variables used to get around @staticmethods
+    current_question = models.IntegerField(initial=1)
+    current_answer = models.FloatField()
 
 
 # PAGES
@@ -40,124 +64,102 @@ class WaitOtherPlayersPage(WaitPage):
     pass
 
 
-class FirstQuestion(Page):
-
+# Main template for question page
+class QuestionPage(Page):
     timeout_seconds = 10
     form_model = "player"
-    form_fields = ["num_1"]
-
-    @staticmethod
-    def before_next_page(player, timeout_happened):
-        correct_answer = 9362
-        error = abs(player.num_1 - correct_answer)
-        punish = correct_answer / 100
-        player.total_payoff += max(100 - error / punish, 0)
-
-    @staticmethod
-    def error_message(player, values):
-        if values["num_1"] < 0:
-            return "Расстояние не может быть отрицательным"
-
-
-class SecondQuestion(Page):
-
-    timeout_seconds = 10
-    form_model = "player"
-    form_fields = ["num_2"]
-
-    @staticmethod
-    def before_next_page(player, timeout_happened):
-        corrent_answer = 2579
-        error = abs(player.num_2 - corrent_answer)
-        punish = corrent_answer / 100
-        player.total_payoff += max(100 - error / punish, 0)
-
-    @staticmethod
-    def error_message(player, values):
-        if values["num_2"] < 0:
-            return "Расстояние не может быть отрицательным"
-
-
-class ThirdQuestion(Page):
-
-    timeout_seconds = 10
-    form_model = "player"
-    form_fields = ["num_3"]
-
-    @staticmethod
-    def before_next_page(player, timeout_happened):
-        corrent_answer = 6814
-        error = abs(player.num_3 - corrent_answer)
-        punish = corrent_answer / 100
-        player.total_payoff += max(100 - error / punish, 0)
-
-    @staticmethod
-    def error_message(player, values):
-        if values["num_3"] < 0:
-            return "Расстояние не может быть отрицательным"
-
-
-class FourthQuestion(Page):
-
-    timeout_seconds = 10
-    form_model = "player"
-    form_fields = ["num_4"]
-
-    @staticmethod
-    def before_next_page(player, timeout_happened):
-        corrent_answer = 7984
-        error = abs(player.num_4 - corrent_answer)
-        punish = corrent_answer / 100
-        player.total_payoff += max(100 - error / punish, 0)
-
-    @staticmethod
-    def error_message(player, values):
-        if values["num_4"] < 0:
-            return "Расстояние не может быть отрицательным"
-
-
-class FifthQuestion(Page):
-
-    timeout_seconds = 10
-    form_model = "player"
-    form_fields = ["num_5"]
-
-    @staticmethod
-    def before_next_page(player, timeout_happened):
-        correct_answer = 2852
-        error = abs(player.num_5 - correct_answer)
-        punish = correct_answer / 100
-        player.total_payoff += max(100 - error / punish, 0)
-        player.total_payoff = round(player.total_payoff)
-
-    @staticmethod
-    def error_message(player, values):
-        if values["num_5"] < 0:
-            return "Расстояние не может быть отрицательным"
-
-
-class Results(Page):
-    pass
-
-
-class CombinedResults(Page):
-    @staticmethod
-    def is_displayed(player):
-        return player.round_number == Constants.num_rounds
 
     @staticmethod
     def vars_for_template(player):
-        return {"total_payoff": player.total_payoff}
+        return {
+            "round_number": player.current_question,
+            "current_question": Constants.questions[player.current_question - 1],
+        }
+
+    @staticmethod
+    def error_message(player, values):
+        if values["num_{}".format(player.current_question)] < 0:
+            return "Расстояние должно быть положительным"
+
+    @staticmethod
+    def get_form_fields(player):
+        return ["num_{}".format(player.current_question)]
+
+    @staticmethod
+    def before_next_page(player, timeout_happened):
+        player.current_question += 1
+
+
+class ResultsWaitPage(WaitPage):
+    @staticmethod
+    def after_all_players_arrive(group):
+        first, second = group.get_players()
+
+        def compare_errors(first_a, second_a, round_num):
+            correct = Constants.answers[round_num - 1]
+            error_1, error_2 = (abs(correct - first_a), abs(correct - second_a))
+            if error_1 > error_2:
+                first.total_payoff += 100
+            elif error_1 == error_2:
+                first.total_payoff += 50
+                second.total_payoff += 50
+            else:
+                second.total_payoff += 100
+
+        # For some reason u cant just access all player fields at once
+        # so you have to go the long way
+        compare_errors(first.num_1, second.num_1, 1)
+        compare_errors(first.num_2, second.num_2, 2)
+        compare_errors(first.num_3, second.num_3, 3)
+        compare_errors(first.num_4, second.num_4, 4)
+        compare_errors(first.num_5, second.num_5, 5)
+
+
+class DecisionPage(Page):
+    form_model = "player"
+
+    @staticmethod
+    def vars_for_template(player):
+        if player.id_in_group == 1:
+            return {"second_player_payoff": str(500 - player.total_payoff)}
+
+    @staticmethod
+    def get_form_fields(player):
+        if player.id_in_group == 1:
+            return ["options"]
+
+    @staticmethod
+    def is_displayed(player):
+        return player.id_in_group == 1
+
+
+class WaitLeaderPage(WaitPage):
+    @staticmethod
+    def is_displayed(player):
+        return player.id_in_group == 2
+
+
+class FinalPage(Page):
+    @staticmethod
+    def vars_for_template(player):
+        winnings_mapping = {0: 0, 1: 100, 2: 200, 3: 300, 4: 400, 5: 500}
+        if player.id_in_group == 1:
+            return {"winnings": winnings_mapping[player.options]}
+        else:
+            leader = player.get_others_in_group()
+            return {"winnings": 500 - winnings_mapping[leader[0].options]}
 
 
 page_sequence = [
     StartPage,
     WaitOtherPlayersPage,
-    FirstQuestion,
-    SecondQuestion,
-    ThirdQuestion,
-    FourthQuestion,
-    FifthQuestion,
-    Results,
-    CombinedResults,
+    QuestionPage,
+    QuestionPage,
+    QuestionPage,
+    QuestionPage,
+    QuestionPage,
+    ResultsWaitPage,
+    DecisionPage,
+    WaitLeaderPage,
+    FinalPage,
 ]
